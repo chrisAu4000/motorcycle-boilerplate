6b0dcfa18dce1ef20bf747cfdf6aefdab2cbfa86
Christian Auer <s0535956@htw-berlin.de>
2016-12-20 19:08:33 +0100

    deleted unnessecery stuff.

--- src/pages/home/home-index.js
+++ src/pages/home/home-index.js
@@ -9,20 +9,13 @@ const initialListState = [
 , {isChecked: true, hasFocus: false, value: 'motor'}
 , {isChecked: false, hasFocus: false, value: 'cycle'}
 ]
-const randomColor = () => '#'+(Math.random()*0xFFFFFF<<0).toString(16)
 
 const view = (list$) => {
   return list$
 }
 
 const Home = (sources) => {
-  // const initialListState = []
   const list = List(sources, {initialState: initialListState})
-  const dur = 1000;
-  const fade$ = periodic(dur/10, 1)
-    .scan((a, c) => a + c, 1)
-    .take(10)
-    .map(x => x / 10)
   const view$ = view(list.DOM)
   return {
     DOM: view$
